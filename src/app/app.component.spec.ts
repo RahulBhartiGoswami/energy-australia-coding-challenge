13ffe1aa87d193572df902ab9bdfe737deff9e1a
RahulBhartiGoswami <86601628+RahulBhartiGoswami@users.noreply.github.com>
2023-09-20 22:10:05 +1000

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,137 +1,72 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MusicFestival } from './interfaces/ModelFestival';
 import { FestivalDataService } from './service/festival-data.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
-import { of } from 'rxjs/internal/observable/of';
+import { FestivalServiceService } from './service/festival-service.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 describe('AppComponent', () => {
- let component: AppComponent;
- let fixture: ComponentFixture<AppComponent>;
- let festivalService: jasmine.SpyObj<FestivalDataService>;
+  let fixture: ComponentFixture<AppComponent>;
+  let component : AppComponent;
+  let festivalDataService: jasmine.SpyObj<FestivalDataService>;
+  let festivalService: jasmine.SpyObj<FestivalServiceService>;
+
+  beforeEach(()=>{
+    const festivalDataServiceSpy = jasmine.createSpyObj('FestivalDataService',['getMockData']);
+    
+    TestBed.configureTestingModule({
+      declarations:[AppComponent],
+      imports:[HttpClientModule],
+      providers:[
+        {provide:FestivalDataService, useValue:festivalDataServiceSpy},
+        HttpClient
+      ]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    festivalDataService = TestBed.inject(FestivalDataService) as jasmine.SpyObj<FestivalDataService>;
+
+  });
 
 
- const mockData: MusicFestival[] = [
-  {
-    "bands": [
-      {
-        "name": "Propeller",
-        "recordLabel": "Pacific Records"
-      },
-      {
-        "name": "Critter Girls",
-        "recordLabel": "ACR"
-      }
-    ]
-  },
-  {
-    "name": "Trainerella",
-    "bands": [
-      {
-        "name": "Wild Antelope",
-        "recordLabel": "Still Bottom Records"
-      },
-      {
-        "name": "Manish Ditch",
-        "recordLabel": "ACR"
-      },
-      {
-        "name": "Adrian Venti",
-        "recordLabel": "Monocracy Records"
-      },
-      {
-        "name": "YOUKRANE",
-        "recordLabel": "Anti Records"
-      }
-    ]
-  },
-  {
-    "name": "LOL-palooza",
-    "bands": [
-      {
-        "name": "Winter Primates",
-        "recordLabel": ""
-      },
-      {
-        "name": "Frank Jupiter",
-        "recordLabel": "Pacific Records"
-      },
-      {
-        "name": "Jill Black",
-        "recordLabel": "Fourth Woman Records"
-      },
-      {
-        "name": "Werewolf Weekday",
-        "recordLabel": "XS Recordings"
-      }
-    ]
-  },
-  {
-    "name": "Small Night In",
-    "bands": [
-      {
-        "name": "Squint-281",
-        "recordLabel": "Outerscope"
-      },
-      {
-        "name": "The Black Dashes",
-        "recordLabel": "Fourth Woman Records"
-      },
-      {
-        "name": "Green Mild Cold Capsicum",
-        "recordLabel": "Marner Sis. Recording"
-      },
-      {
-        "name": "Yanke East",
-        "recordLabel": "MEDIOCRE Music"
-      },
-      {
-        "name": "Wild Antelope",
-        "recordLabel": "Marner Sis. Recording"
-      }
-    ]
-  },
-  {
-    "name": "Twisted Tour",
-    "bands": [
-      {
-        "name": "Summon",
-        "recordLabel": "Outerscope"
-      },
-      {
-        "name": "Auditones",
-        "recordLabel": "Marner Sis. Recording"
-      },
-      {
-        "name": "Squint-281"
-      }
-    ]
-  }
- ];
-
- beforeEach(()=>{
-  festivalService = jasmine.createSpyObj('festivalService',['getMockData']);
-  //festivalService.getMockData.and.returnValue(mockData);
-  TestBed.configureTestingModule({
-    declarations:[AppComponent],
-    providers:[{provide:FestivalDataService, useValue:festivalService}],
-    imports:[HttpClientTestingModule]
-  }).compileComponents();
-
-  fixture = TestBed.createComponent(AppComponent);
-  component = fixture.componentInstance;
-
- });
-
-  it('should create', ()=>{
+  it('should create the app', ()=>{
     expect(component).toBeTruthy();
-  })
+  });
 
-  it('should load festivals', ()=>{
-    fixture.detectChanges();
+  it('should load festivals and parse data on initialization', ()=>{
+    const mockData = [
+      {
+        "bands": [
+          {
+            "name": "Propeller",
+            "recordLabel": "Pacific Records"
+          },
+          {
+            "name": "Critter Girls",
+            "recordLabel": "ACR"
+          }
+        ]
+      }
+    ];
+
+    festivalDataService.getMockData.and.returnValue(mockData);
+    fixture.detectChanges(); // trigger ngOnInit
+
+    expect(festivalDataService.getMockData).toHaveBeenCalled();
     expect(component.festivals).toEqual(mockData);
-  })
 
+  });
 
+  it('should return an empty list of festivals when loading festivals', ()=>{
+    festivalDataService.getMockData.and.returnValue([]);
+    spyOn(component, 'parseAndOrganizedData').and.callThrough();
+    fixture.detectChanges();
+    expect(component.festivals).toBeLessThan(1);
+    expect(component.festivals).toEqual([]);
+    expect(component.sortedData).toEqual([]);
+  });
+  
 });
